2445f5b04201861e59be1715770915698ba74698
Rasak Akeem Olasukanmi <olasukanmiakeem816@gmail.com>
2025-08-21 10:39:50 +0100

    wk_3 updated

--- python/wk_3/working_with_control_flow.py
+++ python/wk_3/working_with_control_flow.py
@@ -168,7 +168,8 @@ print("Access Granted!")
 
 
 # Understanding while True
-#- The while True: loop is an infinite loop - it keeps running forever until you explicity stop it with a break statement or by exiting the program.
+#- The while True: loop is an infinite loop - it keeps running forever until you explicity stop 
+# it with a break statement or by exiting the program.
 #- It is commonly used when:
 #-- You don't know in advance how many times you want the loop to run.
 #-- You want to keep asking the user for input until a valid condition is met.
@@ -178,3 +179,138 @@ print("Access Granted!")
     # Code block
     # Must include a break statement to stop
 
+# Keep asking the user for a name until they type "exit".
+
+while True:
+    name = input("Enter your name (type 'exit' to quit): ")
+    if name.lower() == "exit":
+        print("Goodbye!")
+        break
+    print(f"Hello, {name}")
+    
+# ---> I used 'break' here. If you don't know what it is or what it is doing, its OK. I will explain it next...
+
+## Useful in chat-like applications, forms, or data entry programs where users may input multiple times until they decide to stop.
+
+
+# Loop Control Statements (break, continue and pass)
+#* These keywords help us control the behavior of loops (for and while). Instead of loops always running all iterations, we can skip steps, stop early, or do nothing intentionally.
+
+#1. break
+# Stops loop immediately. It is used if a condition is met and there's no need to continue looping.
+
+for num in range(1, 10):
+    if num == 5:
+        break
+    print(num)
+    
+#The loop stops completely when num == 5.
+# Stop searching when an item is found.
+# Exit when user input matches a condition.
+# Prevent unnecessary ieractions
+
+#2. continue
+# Skips the current iteraction and moves to the next one. It is used if you want to ignore some values but the loop running.
+
+for num in range(1, 6):
+    if num == 3:
+        continue
+    print(num)
+    
+# 3 is skipped, but the loop continues.
+## Some usecases:
+# Skip invalid data.
+# Ignore unwanted characters (like spaces ina string).
+# Continue running but avoid certain cases, etc.
+
+#3. Pass
+# Does nothing. A placeholder to avoid errors. It is used if you haven't written the code yet but to keep the structure.
+
+for num in range(1, 6):
+    if num == 3:
+        pass    # do nothing for now
+    else:
+        print(num)
+        
+# At num == 3, Python executes pass (nothing happens).
+## Some usecases
+
+# Writting code structure (to fill in later).
+# Placeholders in class/method definitions.
+# Temporarily disable parts of code.
+
+
+# Lets try while True again!!!
+
+while True:
+    print("\nMenu: ")
+    print("1. Say Hello")
+    print("2. Say Goodbye")
+    print("3. Exit")
+    
+    choice = input("Choose an option: ")
+    
+    if choice == "1":
+        print("Hello")
+    elif choice == "2":
+        print("Goodbye")
+    elif choice == "3":
+        print("Exiting program...")
+        break
+    else:
+        print("Invalid choice. Try again.")        # Menu: 1. Say Hello 2. Say Goodbye 3. Exit
+
+
+# Try and use while True for validation
+
+while True:
+    age = input("Enter your age: ")
+    if age.isdigit():
+        print(f"Your age is {age}")
+        break
+    else:
+        print("Invalid input. Please enter a number.")
+        
+        
+        
+# Let's make a guess
+
+secret = "python"
+
+while True:
+    guess = input("Guess the secret word: ")
+    if guess.lower() == secret:
+        print("Correct! You guessed the word.")
+        break
+    else:
+        print("Wrong! Try again.")
+        
+        
+# Do you remember this?
+
+balance = 1000
+
+while True:
+    print("\nATM Menu: ")
+    print("1. Check Balance")
+    print("2. Withdraw")
+    print("3. Exit")
+    
+    choice = input("Enter choice: ")
+    
+    if choice == "1":
+        print(f"Your balance is: {balance}")
+    elif choice == "2":
+        amount = int(input("Enter withdrawal amount: "))
+        if amount <= balance:
+            balance -= amount
+            print(f"Withdrawal successful. New balance: {balance}")
+        else:
+            print("Insufficient funds.")
+    elif choice == "3":
+        print("Thank you for using our ATM. Goodbye!")
+        break
+    else:
+        print("Invalid option. Try again.") 
+        
+        
